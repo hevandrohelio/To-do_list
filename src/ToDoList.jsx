d7a1cf78e21a58497c95d20bcd054919983fa325
Hevandro Hélio <68600487+hevandrohelio@users.noreply.github.com>
2025-07-24 18:48:48 -0300

    change css

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 function ToDoList(){
-    const [tasks, setTasks] = useState(["Comer", "Cagar", "Dormir"]);
+    const [tasks, setTasks] = useState([]);
     const [newTask, setNewTask] = useState("");
 
     function handleInputChange(event){
